b831d95ef2914bde89847a963359cfc064a417fa
APIVerve Team <hello@apiverve.com>
2025-03-28 10:50:50 -0500

    Bug Fixes

---
+++ src/apiverve_advicegenerator/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_advicegenerator',
+    version='1.1.9',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='Advice Generator is a simple tool for generating random pieces of advice. It returns a random piece of advice.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
